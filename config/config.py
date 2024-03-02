4a98cc6f6ab10872b1749fc2a205c95e46686cb2
Bryn Dominique <147934556+BrynDom@users.noreply.github.com>
2024-03-02 15:39:55 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -51,7 +51,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Vygaee/Ar.git",
+    "https://github.com/BrynDom/JooxEnjoy.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
@@ -61,10 +61,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", ""
-)  # Example:- https://t.me/gabutan_escape
+)  # Example:- https://t.me/JooxStream
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", ""
-)  # Example:- https://t.me/aruna_mutual
+)  # Example:- https://t.me/JooxSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
